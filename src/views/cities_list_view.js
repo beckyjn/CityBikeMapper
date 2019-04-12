1f1ada161d8ec36bf2455ca9db99ea64fdbb7dbc
Becky <rebeccajoynielsen@gmail.come>
2019-04-13 00:41:28 +0100

    prints list of cities and bike hire companies

--- src/views/cities_list_view.js
+++ src/views/cities_list_view.js
@@ -2,20 +2,37 @@ const PubSub = require('../helpers/pub_sub.js');
 
 const CityListView = function(container){
   this.container = container;
-  this.allNetworkData = null;
-  this.selectedCountry = null;
+  this.selectedCities = null;
 };
 
 CityListView.prototype.bindEvents = function () {
-  PubSub.subscribe('HireSchemes:all-bike-network-data-ready', (evt) => {
-    const networkObjects = evt.detail;
-    this.allNetworkData = networkObjects.networks;
-    // console.log(this.allNetworkData);
-  });
   PubSub.subscribe('HireSchemes:selected-hire-schemes-ready', (evt) => {
-    console.log(evt.detail);
+    const selectedCountryData = evt.detail;
+    this.selectedCities = selectedCountryData;
+    this.publishCityInfo();
   });
 };
+
+CityListView.prototype.publishCityInfo = function () {
+  this.container.innerHTML= "";
+
+  let cityName = "";
+  let bikeHireLocalName = "";
+
+  const eachCity = this.selectedCities.forEach((city) => {
+    listContainer = document.createElement('ul');
+    this.container.appendChild(listContainer);
+
+    listName = document.createElement('li');
+    listName.textContent = `City Name: ${city.location.city}`;
+    listContainer.appendChild(listName);
+
+    listHireCompany = document.createElement('li');
+    listHireCompany.textContent = `You can rent bikes here from ${city.name}`;
+    listContainer.appendChild(listHireCompany);
+  });
+};
+
 
 
 module.exports = CityListView;
